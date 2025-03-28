e40937db7a00fca622a66d8d4f0c976b4dbe5c5d
ducchung2444@gmail.com <ducchung2049@gmail.com>
2025-03-28 15:11:04 +0700

    Design MainLayout for HomePage

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,102 +1,46 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App 123
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
+    <q-layout view="hHh lpR fFf">
+        <q-header
+            reveal
+            elevated
+            class="bg-primary text-white"
+            height-hint="98"
         >
-          Essential Links
-        </q-item-label>
+            <q-toolbar>
+                <q-toolbar-title>
+                    <q-avatar>
+                        <img
+                            src="https://d1785e74lyxkqq.cloudfront.net/_next/static/v3/f/fbab4f587da2242fbe9858fe3e5ba717.svg"
+                        />
+                    </q-avatar>
+                    Traveloke
+                </q-toolbar-title>
+            </q-toolbar>
 
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+            <q-tabs>
+                <q-route-tab to="/" label="Home" />
+                <q-route-tab to="/hotels" label="Hotels" />
+                <q-route-tab to="/flights" label="Flights" />
+            </q-tabs>
+        </q-header>
 
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+
+        <q-footer reveal elevated class="bg-grey-8 text-white">
+            <q-toolbar>
+                <q-toolbar-title>
+                    <q-avatar>
+                        <img
+                            src="https://d1785e74lyxkqq.cloudfront.net/_next/static/v3/f/fbab4f587da2242fbe9858fe3e5ba717.svg"
+                        />
+                    </q-avatar>
+                    Traveloke
+                </q-toolbar-title>
+            </q-toolbar>
+        </q-footer>
+    </q-layout>
 </template>
 
-<script setup>
-import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
-
-const leftDrawerOpen = ref(false)
-
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value
-}
-</script>
+<script setup></script>
--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,18 +1,28 @@
 const routes = [
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
-  },
+    {
+        path: "/",
+        component: () => import("layouts/MainLayout.vue"),
+        children: [
+            { 
+				path: "", 
+				component: () => import("pages/IndexPage.vue") },
+            {
+                path: "hotels",
+				component: () => import("pages/hotels/IndexPage.vue"),
+            },
+			{
+				path: "flights",
+				component: () => import("pages/flights/IndexPage.vue"),
+			}
+        ],
+    },
 
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    // Always leave this as last one,
+    // but you can also remove it
+    {
+        path: "/:catchAll(.*)*",
+        component: () => import("pages/ErrorNotFound.vue"),
+    },
+];
 
-export default routes
+export default routes;
